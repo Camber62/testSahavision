85328fa213b4fb3371873d97ff429670cf319263
Camber62 <94636483+Camber62@users.noreply.github.com>
2024-11-25 22:47:50 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -34,21 +34,83 @@ export default defineComponent({
   setup() {
     const isModalOpen = ref(false);
     const selectedFolderId = ref<number | null>(null);
-    const folders = ref<Folder[]>([
+   const folders = ref<Folder[]>([
+  {
+    id: 1,
+    name: "Папка 1",
+    children: [
+      { id: 2, name: "Папка 1.1", children: [] },
       {
-        id: 1,
-        name: "Папка 1",
+        id: 3,
+        name: "Папка 1.2",
         children: [
-          { id: 2, name: "Папка 1.1", children: [] },
+          { id: 4, name: "Папка 1.2.1", children: [] },
           {
-            id: 3,
-            name: "Папка 1.2",
-            children: [{ id: 4, name: "Папка 1.2.1", children: [] }],
+            id: 5,
+            name: "Папка 1.2.2",
+            children: [
+              { id: 6, name: "Папка 1.2.2.1", children: [] },
+              { id: 7, name: "Папка 1.2.2.2", children: [] },
+            ],
           },
         ],
       },
-      { id: 5, name: "Папка 2", children: [] },
-    ]);
+    ],
+  },
+  {
+    id: 8,
+    name: "Папка 2",
+    children: [
+      { id: 9, name: "Папка 2.1", children: [] },
+      {
+        id: 10,
+        name: "Папка 2.2",
+        children: [
+          { id: 11, name: "Папка 2.2.1", children: [] },
+          { id: 12, name: "Папка 2.2.2", children: [] },
+        ],
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: "Папка 3",
+    children: [
+      {
+        id: 14,
+        name: "Папка 3.1",
+        children: [
+          { id: 15, name: "Папка 3.1.1", children: [] },
+          { id: 16, name: "Папка 3.1.2", children: [] },
+        ],
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: "Папка 4",
+    children: [
+      { id: 18, name: "Папка 4.1", children: [] },
+      {
+        id: 19,
+        name: "Папка 4.2",
+        children: [
+          { id: 20, name: "Папка 4.2.1", children: [] },
+          { id: 21, name: "Папка 4.2.2", children: [] },
+          {
+            id: 22,
+            name: "Папка 4.2.3",
+            children: [
+              { id: 23, name: "Папка 4.2.3.1", children: [] },
+              { id: 24, name: "Папка 4.2.3.2", children: [] },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 
     // Вычисляем имя выбранной папки
     const selectedFolderName = computed(() => {
